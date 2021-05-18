ed7470527ecd1213971ad601f97d690e5078319c
Julio Hermoza <54555294+Tedraco@users.noreply.github.com>
2021-05-18 13:08:16 -0500

    Initial commit

---
+++ app/src/main/java/com/example/loginjh/Splash.java
@@ -0,0 +1,46 @@
+package com.example.loginjh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.loginjh.R;
+
+public class Splash extends AppCompatActivity {
+
+
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+    //variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //Animación
+        topAnim = AnimationUtils.loadAnimation (this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.botton_animation);
+        //lógica y diseño
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN_DELAY);
+    }}
